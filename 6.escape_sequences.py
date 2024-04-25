ab31358cb3eed52623e021c4467484c957671cb8
harshap0202 <02harshpatil@gmail.com>
2024-04-25 15:18:38 +0530

    new

---
+++ 6.escape_sequences.py
@@ -0,0 +1,15 @@
+# type(), str(), and escape sequences exercises
+# Do all of this in a .py file in Pycharm.
+# 1. Create a variable and assign it a float
+new_var = 735.012
+print(new_var)
+
+# 2. Use print() and type() to print the data type of the variable in the output
+print(type(new_var))
+
+# 3. Use str() on the variable from step 1 and concatenate it with the string " is a float." then use print() to display the result
+print(str(new_var) + " is a float")
+
+# 4. print() the string "Hello, I'm [name], it's nice to meet you!" including quotes 
+#    (you will need to use the \' or \" escape sequence depending on whether you enclose your strings in single quotes or double quotes.)
+print("\"Hello, I'm Harshwardhan, it's nice to meet you!\"")
