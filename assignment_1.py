75fb295fff299cae5fa2c18249bc1d2ad790abc0
harshap0202 <02harshpatil@gmail.com>
2024-04-25 14:53:34 +0530

    first

---
+++ assignment_1.py
@@ -0,0 +1,20 @@
+# Do all of this in a .py file in Pycharm
+# 1. create a variable and assign it an integer
+
+num_var = 123
+print(num_var)
+
+# 2. create a second variable and assign it a float
+
+num_var_2 = 1.2
+print(num_var_2)
+
+# 3. create a third variable and assign it a Boolean value
+
+var_boolean = True
+print(var_boolean)
+
+# 4. re-assign a different integer to the first variable you created 
+num_var = 456
+print(num_var)
+ 
---
+++ assignment_3.py
@@ -0,0 +1,10 @@
+# Do all of this in a .py file in Pycharm.  Make a comment for each line of code you write.
+# 1. create a variable, assign it a value of any data type, then use print() to display its assigned value in the output
+any_var = 123434
+print(any_var)
+
+# 2. use print() to print a value of any data type directly
+print(123.456)
+
+# 3. use print() to display the result of an expression that uses at least 2 math operators
+print(10 + 20 // 5)
