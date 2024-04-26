38b218ed94e927c529073912ed8f191d6e2b3144
harshap0202 <02harshpatil@gmail.com>
2024-04-26 10:18:27 +0530

    commit 3

---
+++ 9.int().py
@@ -0,0 +1,8 @@
+# int() exercise
+# In a Python file, use input() to ask the user to enter an integer that 10 will be added to.  Assign what they type to a variable.
+# print() the sum of the integer they entered and 10.
+
+num = input("Please enter num : ")
+var = int(num)
+
+print(var + 10)
